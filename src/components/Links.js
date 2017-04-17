24dc2e4ade1aaf0fa0ced0387475efb9819ab33f
Pete Karl II <pete.karl@gmail.com>
2017-04-16 23:57:29 -0400

    some media query stuff

--- src/components/Links.js
+++ src/components/Links.js
@@ -7,12 +7,6 @@ import driftLogoSrc from '../assets/logo.png';
 
 const Links = () => (
   <nav>
-    <NavLink to="/random">
-      <img className="nav-icon" src={randomSrc} alt="za" />
-    </NavLink>
-    <NavLink to="/news">
-      <img className="nav-icon" src={newsSrc} alt="news" />
-    </NavLink>
     <NavLink to="/">
       <img
         className="nav-title"
@@ -20,12 +14,20 @@ const Links = () => (
         alt="title"
       />
     </NavLink>
-    <NavLink replace to="/about">
-      <img className="nav-icon" src={pizzaSrc} alt="randomSrc" />
-    </NavLink>
-    <NavLink replace to="/work">
-      <img className="nav-icon" src={driftLogoSrc} alt="drift logo" />
-    </NavLink>
+    <div className="subnav">
+      <NavLink to="/random">
+        <img className="nav-icon" src={randomSrc} alt="za" />
+      </NavLink>
+      <NavLink to="/news">
+        <img className="nav-icon" src={newsSrc} alt="news" />
+      </NavLink>
+      <NavLink replace to="/about">
+        <img className="nav-icon" src={pizzaSrc} alt="randomSrc" />
+      </NavLink>
+      <NavLink replace to="/work">
+        <img className="nav-icon" src={driftLogoSrc} alt="drift logo" />
+      </NavLink>
+    </div>
   </nav>
 );
 
